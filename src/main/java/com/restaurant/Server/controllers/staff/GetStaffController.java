ff1d454df243fb575c9e9143bca19dde308a12f4
krzywus <filip.krzywy@gmail.com>
2017-11-05 11:33:24 +0100

    made server return better format for front-end

--- src/main/java/com/restaurant/Server/controllers/staff/GetStaffController.java
+++ src/main/java/com/restaurant/Server/controllers/staff/GetStaffController.java
@@ -1,14 +1,16 @@
 package com.restaurant.Server.controllers.staff;
 
 import com.restaurant.Server.Service.StaffService;
-import com.restaurant.Server.model.Meal;
 import com.restaurant.Server.model.Staff;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,13 +25,15 @@ public class GetStaffController {
         return this.staffService.findStaffByLastName(lastName);
     }
 
-    @GetMapping(value ={"/{id}"})
-    public Optional<Staff> getStaffById(@PathVariable("id") int id){
+    @GetMapping(value ={"/id"})
+    public Optional<Staff> getStaffById(@RequestParam("id") int id){
         return this.staffService.findStaffById(id);
     }
 
     @GetMapping(value={"/all"})
-    Page<Staff> staffList(Pageable pageable){
-        return this.staffService.findAll(pageable);
+    public List<Staff> staffList(){
+        return this.staffService
+                .findAll(PageRequest.of(0,20))
+                .getContent();
     }
 }
